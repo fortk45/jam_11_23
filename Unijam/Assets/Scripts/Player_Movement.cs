a9c423788c073c46ce6e648be59e2f83c14f62c6
TheDocteurC <151994359+TheDocteurC@users.noreply.github.com>
2023-11-25 18:18:23 +0100

    Add files via upload

--- Unijam/Assets/Scripts/Player_Movement.cs
+++ Unijam/Assets/Scripts/Player_Movement.cs
@@ -11,28 +11,34 @@ public class Player_Movement : MonoBehaviour
     [SerializeField] public float maxHeigt;
     [SerializeField] private bool canJump;
     private Rigidbody rb;
+    [SerializeField] private Animator animator;
+    [SerializeField] private Animator armAnimator;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         canJump = true;
-        maxHeigt = 1f;
+        maxHeigt = 0.75f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(rb.position.y < 1)
-        {
-            transform.position = new Vector3(rb.position.x, 1f,rb.position.z);
-        }
+        animator.SetBool("IsMoving", false);
+        transform.rotation = new Quaternion(0f,0f,0f,0f);
         if(Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector3.left * speed * Time.deltaTime);
+            animator.SetFloat("MoveX", -0.5f);
+            armAnimator.SetFloat("MoveX", -0.5f);
+            animator.SetBool("IsMoving", true);
         }
         if(Input.GetKey(KeyCode.D))
         {
             transform.Translate(Vector3.right * speed * Time.deltaTime);
+            animator.SetFloat("MoveX", 0.5f);
+            armAnimator.SetFloat("MoveX", 0.5f);
+            animator.SetBool("IsMoving", true);
         }
         if((Input.GetKey(KeyCode.W)) && canJump)
         {
@@ -52,7 +58,7 @@ public class Player_Movement : MonoBehaviour
     {
         if (!col.gameObject.CompareTag("Plateform")) return;
         canJump = true;
-        maxHeigt = rb.position.y + 1f;
+        maxHeigt = rb.position.y + 0.75f;
     }
 
     void jump()
@@ -71,3 +77,4 @@ public class Player_Movement : MonoBehaviour
         rb.AddForce(jumpVector, ForceMode.Impulse);
     }
 }
+ 
