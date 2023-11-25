02e74ffa40b86c34a808d4423fadd913d79c7fc1
Orio44 <arnaud@marquis.bzh>
2023-11-25 16:52:51 +0100

    rotation de l'arme

--- Unijam/Assets/Scripts/Player_Movement.cs
+++ Unijam/Assets/Scripts/Player_Movement.cs
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -23,19 +22,19 @@ public class Player_Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        if(Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector3.left * speed * Time.deltaTime);
         }
-        if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        if(Input.GetKey(KeyCode.D))
         {
             transform.Translate(Vector3.right * speed * Time.deltaTime);
         }
-        if((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) && canJump)
+        if((Input.GetKey(KeyCode.W)) && canJump)
         {
             jump();
         }
-        if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W))
+        if (Input.GetKeyUp(KeyCode.W))
         {
             canJump = false;
         }
@@ -68,77 +67,3 @@ public class Player_Movement : MonoBehaviour
         rb.AddForce(jumpVector, ForceMode.Impulse);
     }
 }
- 
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Player_Movement : MonoBehaviour
-{
-    // Les valeurs sont choisies de manière arbitraire pour l'instant
-    [SerializeField] public float speed = 8f;
-    [SerializeField] public float jumpForce = 5f;
-    [SerializeField] public float maxHeigt;
-    [SerializeField] private bool canJump;
-    private Rigidbody rb;
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.rotation = new Quaternion(0f,0f,0f,0f);
-        //transform.position.z = new float 0f;
-        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
-        }
-        if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
-        }
-        if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
-        {
-            jump();
-        }
-        if(rb.position.y > maxHeigt)
-        {
-            stopJump();
-        }
-    }
-
-    void OnCollisionEnter(Collision col)
-    {
-        if (!col.gameObject.CompareTag("Plateform")) return;
-        canJump = true;
-        maxHeigt = rb.position.y + 3f;
-    }
-
-    void jump()
-    {
-        if(canJump)
-        {
-            rb.AddForce(Vector3.up * jumpForce);
-        }
-        else
-        {
-            stopJump();
-        }
-    }
-
-    void stopJump()
-    {
-        if(canJump == false)
-        {}
-        else
-        {
-            canJump = false;
-            rb.velocity = Vector3.zero;
-        }
-    }
-}
->>>>>>> Stashed changes
